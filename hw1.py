6c261d9d7352b2c1ce54fbc49892e8b7312e9622
Iryna Sadovska <irynasadovskay@gmail.com>
2020-04-20 15:03:39 +0300

    Added hw1.py

---
+++ hw1.py
@@ -0,0 +1,19 @@
+print(" *\t* * *     *       * *       *         *        "
+    "\n *\t*     *    *     *  **      *        * *       "
+    "\n *\t*      *    *   *   * *     *       *   *      "
+    "\n *\t*     *      * *    *  *    *      *     *     "
+    "\n *\t* * *         *     *   *   *     *       *    "
+    "\n *\t* *           *     *    *  *    ***********   "
+    "\n *\t*   *         *     *     * *   *           *  "
+    "\n *\t*     *       *     *      **  *             * "
+    "\n *\t*       *     *     *       * *               *")
+
+
+print("Escape sequences"
+      "\n\\a      Bell (alert)"
+      "\n\\b      Backspace"
+      "\n\\n      New line"
+      "\n\\t      Horizontal tab"
+      "\n\\\      Backslash \\"
+      "\n\\\"      Double quotation mark \""
+      "\n\\\'      Single quotation mark '")
